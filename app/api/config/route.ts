2433f20ef6c767dcf4fccb1e0fe5d0f4a58b40a0
zocvee <jqunhu@gmail.com>
2026-08-08 17:44:31 +0800

    Update route.ts

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -4,6 +4,20 @@ import { getServerSideConfig } from "../../config/server";
 
 // Danger! Do not hard code any secret value here!
 // 警告！不要在这里写入任何敏感信息！
+const DANGER_CONFIG = {
+  needCode: true,
+  hideUserApiKey: false,
+  disableGPT4: false,
+  hideBalanceQuery: false,
+  disableFastLink: false,
+  customModels: "",
+  defaultModel: "",
+  visionModels: "",
+};
+
+declare global {
+  type DangerConfig = typeof DANGER_CONFIG;
+}
 
 async function handle() {
   // 每次请求时实时读取环境变量，确保获取最新配置
@@ -17,6 +31,7 @@ async function handle() {
     customModels: serverConfig.customModels,
     defaultModel: serverConfig.defaultModel,
     visionModels: serverConfig.visionModels,
+    // 管理员配置了哪些 AI 服务商的 API Key
     availableProviders: {
       OpenAI: !!serverConfig.apiKey,
       Azure: serverConfig.isAzure,
@@ -37,9 +52,7 @@ async function handle() {
   });
 }
 
-declare global {
-  type DangerConfig = Awaited<ReturnType<typeof handle>>;
-}
-
 export const GET = handle;
 export const POST = handle;
+
+export const runtime = "edge";
