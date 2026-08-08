2f9e8de8b3cd9d82f35ec0bbed3a79387af41431
zocvee <jqunhu@gmail.com>
2026-08-08 18:38:11 +0800

    Update route.ts

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 import { getServerSideConfig } from "../../config/server";
+
 // Danger! Do not hard code any secret value here!
 // 警告！不要在这里写入任何敏感信息！
 const DANGER_CONFIG = {
