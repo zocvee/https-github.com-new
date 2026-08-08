a4744a21fd5d2a7db9e4e16c19edfcf1a33ab7c6
zocvee <jqunhu@gmail.com>
2026-08-08 22:42:39 +0800

    Update api.ts

--- app/client/api.ts
+++ app/client/api.ts
@@ -76,7 +76,7 @@ export interface ChatOptions {
   messages: RequestMessage[];
   config: LLMConfig;
 
-  onUpdate?: (message: string, chunk: string) => void;
+  onUpdate?: (message: string, chunk: string, thinkingInfo?: { thinkingContent: string; thinkingDuration: number; thinkingFinished: boolean }) => void;
   onFinish: (message: string, responseRes: Response) => void;
   onError?: (err: Error) => void;
   onController?: (controller: AbortController) => void;
